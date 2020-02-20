82476474e9673da89f8206a72a78eade4ae2af08
luca-rosa <luca.rosa@ucl.ac.uk>
2020-02-20 16:16:39 +0000

    load project

---
+++ functionsDiffusion.py
@@ -0,0 +1,96 @@
+import numpy as np
+import scipy
+from scipy.ndimage import laplace
+from scipy.integrate import odeint, solve_ivp
+import matplotlib.pyplot as plt
+from matplotlib import cm, animation, rc
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from mpl_toolkits.mplot3d import Axes3D
+from IPython.display import HTML
+import time
+import os
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+def ficks(s, w):
+    return(laplace(s) / np.power(w, 2))
+
+
+def hill(s, K, lam):
+    h = s**lam / (K**lam + s**lam)
+    return(h)
+
+
+def nutrient(N_matrix):
+    return (hill(N_matrix, 80, 2))
+
+
+def multi_plots(sim, title=""):
+    f, ax = plt.subplots(3, 3, sharex=True, sharey=False, figsize=(15, 15))
+
+    f.suptitle(title, fontsize=40)
+    im1 = ax[0, 0].imshow(sim[3], interpolation="none", cmap=cm.viridis, vmin=0, vmax=1)
+    ax[0, 0].set_title("Sender")
+    divider = make_axes_locatable(ax[0, 0])
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im1, cax=cax, shrink=0.8)
+
+    im2 = ax[0, 1].imshow(sim[5], interpolation="none", cmap=cm.viridis, vmin=0, vmax=1)
+    ax[0, 1].set_title("Receiver")
+    divider = make_axes_locatable(ax[0, 1])
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im2, cax=cax, shrink=0.8)
+
+    im3 = ax[0, 2].imshow(sim[1], interpolation="none", cmap=cm.viridis, vmin=0, vmax=5)
+    ax[0, 2].set_title("Arabinose")
+    divider = make_axes_locatable(ax[0, 2])
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im3, cax=cax, shrink=0.8)
+
+    im4 = ax[1, 0].imshow(sim[3], interpolation="none", cmap=cm.viridis, vmin=0)
+    ax[1, 0].set_title("LuxI")
+    divider = make_axes_locatable(ax[1, 0])
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im4, cax=cax, shrink=0.8)
+
+    im5 = ax[1, 1].imshow(sim[4], interpolation="none", cmap=cm.viridis, vmin=0)
+    ax[1, 1].set_title("C6")
+    divider = make_axes_locatable(ax[1, 1])
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im5, cax=cax, shrink=0.8)
+
+    im6 = ax[1, 2].imshow(sim[6], interpolation="none", cmap=cm.viridis, vmin=0)
+    ax[1, 2].set_title("GFP")
+    divider = make_axes_locatable(ax[1, 2])
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im6, cax=cax, shrink=0.8)
+
+    im7 = ax[2, 0].imshow(sim[2], interpolation="none", cmap=cm.viridis, vmin=0, vmax=100)
+    ax[2, 0].set_title("Nutrients")
+    divider = make_axes_locatable(ax[2, 0])
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    f.colorbar(im7, cax=cax, shrink=0.8)
+
+    ax[2, 1].axis('off')
+    ax[2, 2].axis('off')
+
+    return(f)
+
+
+def plots(sim, names):
+
+    n_plots = sim.shape[0]
+    x = int(np.ceil(n_plots / 3))
+
+    f, ax = plt.subplots(x, 3, sharex=True, sharey=False, figsize=(15, 15))
+
+    for i, val in enumerate(ax.flatten()):
+
+        if i < n_plots:
+            im = ax.flatten()[i].imshow(sim[i], cmap=cm.viridis, vmin=0)
+            ax.flatten()[i].set_title(names[i])
+            divider = make_axes_locatable(ax.flatten()[i])
+            cax = divider.append_axes("right", size="5%", pad=0.05)
+            f.colorbar(im, cax=cax, shrink=0.8, label='')
+        else:
+            ax.flatten()[i].axis("off")
---
+++ mainDiffusion.py
@@ -0,0 +1,122 @@
+from functionsDiffusion import *
+
+
+def main():
+    def model_small(t, U_flat, shape):
+        x_s = 20  # synthase production rate(au/min)
+        x_a = 0.1
+        x_g = .1
+        lambda_a = 2.3  # hill coeff ahl2
+        K_a = 40  # M and M constant for ahl2
+        D = 0.03  # AHL diffusion coeff (#mm2/min)
+        D_a = .3
+        w = 0.75
+        rho_n = 3
+        rc = 6 * 0.0001
+        Dc = 0.0001
+        rho = 5 * 0.001
+
+        U_grid = U_flat.reshape(shape)
+        N = nutrient(U_grid[2])
+
+        LuxI_ficks = ficks(U_grid[0], w)
+        arabinose_ficks = ficks(U_grid[1], w)
+        n_ficks = ficks(U_grid[2], w)
+        S_ficks = ficks(U_grid[3], w)
+        c6_ficks = ficks(U_grid[4], w)
+        R_ficks = ficks(U_grid[5], w)
+        gfp = ficks(U_grid[6], w)
+
+        S = Dc * S_ficks + rc * N * U_grid[3]
+        R = Dc * R_ficks + rc * N * U_grid[5]
+        LuxI = x_s * N * hill(U_grid[1], lambda_a, K_a) * U_grid[3]
+        c6 = D_a * c6_ficks + (x_a * U_grid[0]) - rho * U_grid[4]
+        arabinose = D * arabinose_ficks
+        n = D * n_ficks - rho_n * N * (U_grid[3] + U_grid[5])
+        gfp = x_g * N * hill(U_grid[4], lambda_a, K_a) * U_grid[5]
+
+        return(np.concatenate((LuxI.flatten(), arabinose.flatten(), n.flatten(),
+                               S.flatten(), c6.flatten(), R.flatten(),
+                               gfp.flatten())))
+
+    grid_size = 900
+    U = np.zeros(grid_size * 7)
+    U = U.reshape(7, 30, 30)
+
+    shape = U.shape
+    size = U.size
+
+    U[2] = 100
+
+    U[3][14, 14] = 0.5
+    U[3][14, 15] = 0.5
+    U[3][15, 14] = 0.5
+    U[3][15, 15] = 0.5
+
+    U[1][14, 14] = 5
+    U[1][14, 15] = 5
+    U[1][15, 14] = 5
+    U[1][15, 15] = 5
+
+    i = 0
+    for j in np.arange(1, 4):
+        U[5][11 - i, 14] = 0.5
+        U[5][11 - i, 15] = 0.5
+        U[5][12 - i, 14] = 0.5
+        U[5][12 - i, 15] = 0.5
+        i = i + 3
+    i = 0
+    for j in np.arange(1, 4):
+        U[5][17 + i, 14] = 0.5
+        U[5][17 + i, 15] = 0.5
+        U[5][18 + i, 14] = 0.5
+        U[5][18 + i, 15] = 0.5
+        i = i + 3
+
+    i = 0
+    for j in np.arange(1, 4):
+        U[5][14, 11 - i] = 0.5
+        U[5][14, 12 - i] = 0.5
+        U[5][15, 11 - i] = 0.5
+        U[5][15, 12 - i] = 0.5
+        i = i + 3
+
+    i = 0
+    for j in np.arange(1, 4):
+        U[5][14, 17 + i] = 0.5
+        U[5][14, 18 + i] = 0.5
+        U[5][15, 17 + i] = 0.5
+        U[5][15, 18 + i] = 0.5
+        i = i + 3
+
+    t_final = 1000  # mins
+    dt = .1
+    t_points = int(t_final / dt)
+
+    t = np.arange(0, t_final, dt)
+    U_init = U.flatten()
+    start_time = time.time()
+
+    sim_ivp = solve_ivp(model_small, [0, t_final], U_init,
+                        t_eval=t, args=(shape,))
+
+    sim_ivp = sim_ivp.y.reshape(7, 30, 30, t_points)
+
+    if os.path.isdir(os.getcwd() + "/simulation_pdfs") is False:
+        print("ciao")
+        os.mkdir(os.getcwd() + "/simulation_pdfs")
+
+    with PdfPages("simulation_pdfs/simulation_" + time.strftime("%H%M%S") + ".pdf") as pdf:
+
+        for i in np.arange(0, 901, 120):
+            t = int(i / 60)
+            f1 = multi_plots(sim_ivp[:, :, :, i], str(t) + " hours")
+            pdf.savefig()
+            # plt.show()
+            plt.close()
+
+    end_time = time.time()
+    print(end_time - start_time)
+
+
+main()
