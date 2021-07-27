e24512ddb7d39697178cfce5584d30b660f136af
ajfedorec <ajfedorec@users.noreply.github.com>
2021-07-27 10:11:06 +0100

    fix Monod error

--- example_bandpass.py
+++ example_bandpass.py
@@ -20,19 +20,19 @@ def main():
 
     ## experimental parameters
     D_A = 1e-4 / w ** 2  # mm^2 per min ***** IPTG DIFFUSION RATE
-    T7_0 = 1  # ***** a.u. initial T7RNAP concentration per cell
-    R_0 = 1  # ***** a.u. initial REPRESSOR concentration per cell
-    GFP_0 = 1  # a.u. ***** initial GFP concentration per cell
-    A_0 = 10e-3 * 1e-6 * 6.022e23  # ***** initial inducer concentration - concentration * volume * Avogadro
-    inducer_positions = [[8, 8], [11, 13]]  # positions specified on 384 well plate [[row], [col]]
+    T7_0 = 1e0 # ***** a.u. initial T7RNAP concentration per cell
+    R_0 = 1e0  # ***** a.u. initial REPRESSOR concentration per cell
+    GFP_0 = 1e0  # a.u. ***** initial GFP concentration per cell
+    A_0 = 2.5e-3 * 1e-6 * 6.022e23  # ***** initial inducer concentration - concentration * volume * Avogadro
+    inducer_positions = [[8, 8], [10, 15]]  # positions specified on 384 well plate [[row], [col]]
     X_0 = 0.3 * 1e8 * 10 / (environment_size[0] * environment_size[1])  # ***** initial cell count per grid position - 0.3 OD in 10mL agar ~ 0.3 * 1e8 * 10 / (environment size)
-    N_0 = 0.04 / (environment_size[0] * environment_size[1])  # g ***** initial nutrient per grid position - 0.4% = 0.4g per 100 mL = 0.04 / (environment_size)
+    N_0 = 0.04 / (environment_size[0] * environment_size[1])  # g ***** initial nutrient per grid position - 0.4% = 0.4g per 100 mL -> in 10mL agar = 0.04 / (environment_size)
 
     ## growth parameters (currently Monod growth but can be replaced with fitted growth curves)
     D_N = 1e-4 / w**2  # mm^2 per min  ***** nutrient diffusion rate
     mu_max = 0.02  # per min  *****  max growth rate
-    K_mu = 1  # g  ***** growth Michaelis-Menten coeffecient
-    gamma = 1E12  # cells per g  ***** yield
+    K_mu = 6e-3  # g  ***** growth Michaelis-Menten coeffecient
+    gamma = 1E8  # cells per g  ***** yield
 
     ## From Zong paper
     alpha_T = 6223  #
@@ -65,8 +65,13 @@ def main():
         alpha_R, beta_R, K_IR, n_IR, K_lacR, R_0, \
         alpha_G, beta_G, n_A, K_A, n_R, K_R = params
 
-        n = D_N * hf.ficks(species['N'], w) - mu_max * K_mu / (K_mu + species['N']) * species['X'] / gamma
-        return n
+        mu = mu_max * species['N'] / (K_mu + species['N'])
+        # Xt = A + (K - A) / (C + Q * exp(-B * t))**(1/v)
+        # Xdt = A + (K - A) / (C + Q * exp(-B * (t+dt))) ** (1 / v)
+        # mu = (Xdt - Xt) / (dt * Xt)
+
+        dN = D_N * hf.ficks(species['N'], w) - mu * species['X'] / gamma
+        return dN
     N.set_behaviour(N_behaviour)
     plate.add_species(N)
 
@@ -80,8 +85,13 @@ def main():
         alpha_R, beta_R, K_IR, n_IR, K_lacR, R_0, \
         alpha_G, beta_G, n_A, K_A, n_R, K_R = params
 
-        x = mu_max * K_mu / (K_mu + species['N']) * species['X']
-        return x
+        mu = mu_max * species['N'] / (K_mu + species['N'])
+        # Xt = X_0 + (X_max - X_0) / (1 + Q * exp(- mu_max * t)) ** (1 / v)
+        # Xdt = X_0 + (X_max - X_0) / (1 + Q * exp(- mu_max * (t + dt))) ** (1 / v)
+        # mu = (Xdt - Xt) / (dt * Xt)
+
+        dX = mu * species['X']
+        return dX
     strain.set_behaviour(X_behaviour)
     plate.add_species(strain)
 
@@ -99,8 +109,8 @@ def main():
         alpha_R, beta_R, K_IR, n_IR, K_lacR, R_0, \
         alpha_G, beta_G, n_A, K_A, n_R, K_R = params
 
-        a = D_A * hf.ficks(species['A'], w)
-        return a
+        dA = D_A * hf.ficks(species['A'], w)
+        return dA
     A.set_behaviour(A_behaviour)
     plate.add_species(A)
 
@@ -114,11 +124,14 @@ def main():
         alpha_R, beta_R, K_IR, n_IR, K_lacR, R_0,\
         alpha_G, beta_G, n_A, K_A, n_R, K_R = params
 
-        mu = mu_max * K_mu / (K_mu + species['N'])
+        mu = mu_max * species['N'] / (K_mu + species['N'])
+        # Xt = A + (K - A) / (C + Q * exp(-B * t)) ** (1 / v)
+        # Xdt = A + (K - A) / (C + Q * exp(-B * (t + dt))) ** (1 / v)
+        # mu = (Xdt - Xt) / (dt * Xt)
 
         T7 = alpha_T + beta_T - alpha_T * K_lacT / (1 + (species['A'] / K_IT)**n_IT + K_lacT) + T7_0 * np.exp(-mu * t)
-        R = alpha_R + beta_R - alpha_R * K_lacR / (1 + (species['A'] / K_IR)**n_IR + K_lacR) + R_0 * np.exp(-mu * t)
-        # R = 0  # produces treshold
+        # R = alpha_R + beta_R - alpha_R * K_lacR / (1 + (species['A'] / K_IR)**n_IR + K_lacR) + R_0 * np.exp(-mu * t)
+        R = 0  # produces treshold
 
         dGFP = (alpha_G * mu * T7**n_A / (K_A**n_A + T7**n_A) * K_R**n_R / (K_R**n_R + R**n_R) + beta_G * mu - species['G'] * mu)
 
@@ -131,23 +144,29 @@ def main():
               alpha_T, beta_T, K_IT, n_IT, K_lacT, T7_0,
               alpha_R, beta_R, K_IR, n_IR, K_lacR, R_0,
               alpha_G, beta_G, n_A, K_A, n_R, K_R)
-    sim = plate.run(t_final = 12*60 + 1,
+    sim = plate.run(t_final = 1250 + 1,
                     dt = 1,
                     params = params)
 
     ## plotting
-    plate.plot_simulation(sim, 3, 'log10', 2)
+    # plate.plot_simulation(sim, 7, 'log10', 2)
 
-    # calculate total GFP (rather than GFP per cell) = X * GFP
-    # plate_view = sim[1] * sim[3]
-    #
-    # import matplotlib.pyplot as plt
-    # from matplotlib import cm
-    #
+    ## calculate total GFP (rather than GFP per cell) = X * GFP
+    plate_view = sim[1] * sim[3]
+
+    ## plot
+    import matplotlib.pyplot as plt
+    from matplotlib import cm
+
+    timpoints = 100
+    tps = np.linspace(0, plate_view.shape[2] - 1, timpoints)
+
+    plt.plot(tps, np.log10(plate_view[15, 19:29, tps.astype(int)]))
+    plt.show()
+
     # timpoints = 10
-    # fig, axs = plt.subplots(timpoints, sharex='all', sharey='all')
-    #
     # tps = np.linspace(0, plate_view.shape[2] - 1, timpoints)
+    # fig, axs = plt.subplots(timpoints, sharex='all', sharey='all')
     # for idx, ax in enumerate(axs.flatten()):
     #     im = ax.imshow(plate_view[:, :, int(tps[idx])],
     #                    interpolation="none",
@@ -158,7 +177,7 @@ def main():
     #     ax.set_ylabel(int(tps[idx]))
     # fig.show()
 
-    ## make video of GFP over time
+    # # make video of GFP over time
     # import matplotlib.animation as animation
     # fig, ax = plt.subplots()
     # ims = []
